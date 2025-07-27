76a823069a1aa48cf879fab1b7a1819ef1320501
Dr. Dean von Schoultz <deanvons@gmail.com>
2025-07-27 12:51:00 +0200

    CHORE: renamed Name to name

--- src/main/java/no/loopacademy/models/Survivor.java
+++ src/main/java/no/loopacademy/models/Survivor.java
@@ -2,17 +2,17 @@ package no.loopacademy.models;
 
 public class Survivor {
 
-    private String Name;
+    private String name;
 
     public Survivor(String name) {
-        this.Name = name;
+        this.name = name;
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public void setName(String name) {
-        Name = name;
+        name = name;
     }
 }
